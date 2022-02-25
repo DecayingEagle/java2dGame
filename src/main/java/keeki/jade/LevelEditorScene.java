258534554176210739cfaf139597203b3f04012c
Keeki <therealeagle.yt@gmail.com>
2022-02-25 21:15:14 +0100

    changes to import

--- src/main/java/keeki/jade/LevelEditorScene.java
+++ src/main/java/keeki/jade/LevelEditorScene.java
@@ -10,24 +10,6 @@ import org.lwjgl.BufferUtils;
 import keeki.renderer.Shader;
 
 public class LevelEditorScene extends Scene {
-    
-    private String vertexShaderSrc =    "#version 330 core\n"+
-                                        "layout (location=0) in vec3 aPos;\n"+
-                                        "layout (location=1) in vec4 aColor;\n"+
-                                        "out vec4 fColor;\n"+
-                                        "void main() {\n"+
-                                            "fColor = aColor;\n"+
-                                            "gl_Position = vec4(aPos, 1.0);\n"+
-                                        "}\n";
-
-    private String fragmentShaderSrc =  "#version 330 core\n"+
-                                        "in vec4 fColor;\n"+
-                                        "out vec4 color;\n"+
-                                        "void main(){\n"+
-                                            "color = fColor;\n"+
-                                        "}\n";
-
-    private int vertexID, fragmentID, shaderProgram;
 
     private float[] vertexArray = {
         // position             // color
--- src/main/java/keeki/jade/Window.java
+++ src/main/java/keeki/jade/Window.java
@@ -1,18 +1,12 @@
 package keeki.jade;
 
-import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
 
-import java.nio.*;
-
-import javax.swing.plaf.metal.MetalIconFactory.FolderIcon16;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 import util.*;
